317cbbcd89d7d41862379ad8021e92c19f80f459
sanket <mail.sanket10345@gmail.com>
2025-02-24 22:42:09 +0530

    StringCalculator in add method extract comma speetated numbers and add their sum Wrote test cases

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,10 @@
 // stringCalculator.js
 class StringCalculator {
     add(numbers) {
-      return 0;
+        if (!numbers) return 0;
+
+        const numArray = numbers.split(",").map(Number);
+        return numArray.reduce((sum, num) => sum + num, 0);
     }
   }
   
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -10,5 +10,10 @@ describe('StringCalculator', () => {
   test('returns 0 for an empty string', () => {
     expect(calculator.add("")).toBe(0);
   });
-
+  test('returns sum of a single number', () => {
+    expect(calculator.add("1")).toBe(1);
+  });
+  test('returns sum of two numbers', () => {
+    expect(calculator.add("1,5")).toBe(6);
+  });
 });
